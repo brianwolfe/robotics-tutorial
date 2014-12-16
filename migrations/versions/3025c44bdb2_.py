22585d29220709dc3a3de16b03c626ca27c715ca
Brian Wolfe <bwolfe@irobot.com>
2014-12-16 17:31:52 -0500

    Add migration version? Not sure if this is right

---
+++ migrations/versions/3025c44bdb2_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 3025c44bdb2
+Revises: None
+Create Date: 2014-12-16 12:13:55.759378
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3025c44bdb2'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
