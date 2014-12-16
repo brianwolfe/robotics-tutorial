3e840d6c06c20120846856f87f915b8ef98afcc3
Brian Wolfe <bwolfe@irobot.com>
2014-12-16 08:33:16 -0500

    add some commenting about serving of static files

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-# from models import *
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+# Sending static assets from Flask directly.
+# I'll update this to send from CDN URLs and
+# S3 URLs eventually, but this works for now.
 @app.route('/static/<path:filename>')
 def get_static_assets(filename):
     return send_from_directory('/static/', filename)
