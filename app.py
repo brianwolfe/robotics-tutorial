95e21b44ab91cea706c745d103521f21ffe83284
Brian Wolfe <bwolfe@irobot.com>
2015-01-14 16:51:40 -0500

    Fix up Unscented Kalman Filter implementation
    - Unscented Kalman filter appears to be correctly implemented now

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 import os.path
 import sys
-from urllib.parse import urljoin
-from numpy import (array, concatenate, float64, cos, sin, exp, mean,
-    diag)
+# from urllib.parse import urljoin
+from numpy import (array, concatenate, float64, cos, sin,
+                   diag, ones)
 from numpy.linalg import cholesky, inv
 
 cmd_folder = os.path.dirname(os.path.abspath(__file__))
 if cmd_folder not in sys.path:
-     sys.path.insert(0, cmd_folder)
+    sys.path.insert(0, cmd_folder)
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -20,6 +20,7 @@ tutorial_path = os.path.join(cmd_folder, 'tutorials')
 tutorials = []
 tutorial_pages = {}
 
+
 def load_tutorials():
     global tutorials
     global tutorial_pages
@@ -28,22 +29,24 @@ def load_tutorials():
     for tutorial in tutorials:
         # cut off .html
         if tutorial[-5:] == '.html':
-            print("loading tutorial: ", tutorial)
             tutorial_name = tutorial[:-5]
             tutorial_pages[tutorial_name] = \
-                    open(os.path.join(tutorial_path, tutorial)).read()
+                open(os.path.join(tutorial_path, tutorial)).read()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
 
 @app.route('/tutorial/<path:tutorial>', methods=['GET'])
 def tutorial(tutorial):
     if app.config['DEBUG']:
         load_tutorials()
     if tutorial in tutorial_pages:
-        jsname = app.config['STATIC_URL'] +  '/js/' + tutorial + '.js'
-        return render_template('tutorial.html',
+        jsname = app.config['STATIC_URL'] + '/js/' + tutorial + '.js'
+        return render_template(
+            'tutorial.html',
             body=tutorial_pages[tutorial],
             jsname=jsname)
     else:
@@ -53,15 +56,16 @@ def tutorial(tutorial):
 # Sending static assets from Flask directly.
 # I'll update this to send from CDN URLs and
 # S3 URLs eventually, but this works for now.
-@app.route('/static/<path:filename>')
-def get_static_assets(filename):
-    return send_from_directory('/static/', filename)
+# @app.route('/static/<path:filename>')
+# def get_static_assets(filename):
+#     return send_from_directory('/static/', filename)
 
 
 ROBOT_RADIUS = 1
 UKF_WEIGHT_ALPHA = 1.0
-UKF_WEIGHT_BETA = 0.0
+UKF_WEIGHT_BETA = 2.0
 UKF_WEIGHT_KAPPA = 1.0
+
 
 def ukf_measurement(x, y, Q, robot):
     # Previous mean
@@ -77,33 +81,37 @@ def ukf_measurement(x, y, Q, robot):
     # Make diagonal version of weights to make algebra easier
     wm_diag, wc_diag = diag(wm), diag(wc)
 
+
     # Expected measurements from sigmapoints
     Z_points = sigmapoints.dot(array([[1, 0], [0, 1], [0, 0]]))
 
     # Mean of expected measurements
     z_mean = wm.dot(Z_points)
+
     # Covariance of expected measurements
-    S_cov = (Z_points - z_mean).T.dot(wm_diag).dot(Z_points - z_mean) + Q
+    S_cov = (Z_points - z_mean).T.dot(wc_diag).dot(Z_points - z_mean) + Q
     # Cross-covariance of position and expected measurements
     sigma_xz = (sigmapoints - mu).T.dot(wc_diag).dot(Z_points - z_mean)
     # Kalman gain
     K = sigma_xz.dot(inv(S_cov))
 
     # Updated mean
-    mu_new = mu + K.dot(z_mean - z)
+    mu_new = mu + K.dot(z - z_mean)
     # Updated covariance
-    sigma_new = sigma - K.dot(S_cov).dot(K.T)
+    sigma_new = sigma - sigma_xz.dot(K.T)
     # Compute the updated sigmapoints
     sigmapoints_new, wm, wc = ukf_get_sigmapoints(mu_new, sigma_new)
 
     robot = {
-            'mu': mu_new.reshape((-1, 1)).tolist(),
-            'sigma': sigma_new.tolist(),
-            'previous_sigmapoints': sigmapoints.tolist(),
-            'current_sigmapoints': sigmapoints_new.tolist()
-            }
+        'mu': mu_new.reshape((-1, 1)).tolist(),
+        'sigma': sigma_new.tolist(),
+        'type': 'ukf',
+        'previous_sigmapoints': sigmapoints.tolist(),
+        'current_sigmapoints': sigmapoints_new.tolist()
+        }
 
     return robot
+
 
 @app.route('/measurementupdate/<path:filtertype>', methods=['POST'])
 def measurement_update_request(filtertype='ukf'):
@@ -126,17 +134,8 @@ def measurement_update_request(filtertype='ukf'):
 
     return jsonify({'robots': new_robots})
 
+
 def ukf_get_sigmapoints(mu, sigma):
-    mu.reshape((-1,))
-
-    sigma_offsets = cholesky(sigma)
-    # Scale offsets?
-
-    sigmapoints = concatenate([
-        [mu],
-        mu - sigma_offsets,
-        mu + sigma_offsets
-        ])
 
     alpha = UKF_WEIGHT_ALPHA
     beta = UKF_WEIGHT_BETA
@@ -145,41 +144,53 @@ def ukf_get_sigmapoints(mu, sigma):
     n = sigma.shape[0]
     ukf_lambda = alpha**2 * (n + kappa) - n
 
+    mu = mu.reshape((-1,))
+    sigma_offsets = cholesky((n + ukf_lambda) * sigma).T
+
+    sigmapoints = concatenate([
+        [mu],
+        mu - sigma_offsets,
+        mu + sigma_offsets
+        ])
+
     w0m = ukf_lambda / (n + ukf_lambda)
     w0c = ukf_lambda / (n + ukf_lambda) + (1 - alpha**2 + beta)
     wi = 1 / (2 * (n + ukf_lambda))
     wm = array([w0m] + [wi]*(sigmapoints.shape[0] - 1))
     wc = array([w0c] + [wi]*(sigmapoints.shape[0] - 1))
+    wc_diag = diag(wc)
 
     return sigmapoints, wm, wc
 
-def ukf_movement(left_wheel, right_wheel, robot):
+
+def ukf_movement(left_wheel, right_wheel, R, robot):
     mean = array(robot['mu'], dtype=float64)
     cov = array(robot['sigma'], dtype=float64)
 
-    dd = (left_wheel + right_wheel) / 2;
+    dd = (left_wheel + right_wheel) / 2
     dt = (right_wheel - left_wheel) / ROBOT_RADIUS
 
-    sigmapoints, wm, wc = ukf_get_sigmapoints(mean, cov)
+    xi, wm, wc = ukf_get_sigmapoints(mean, cov)
 
-    wm_diag, wc_diag = diag(wm), diag(wc)
+    wc_diag = diag(wc)
 
-    x1 = mean[0]
-    y1 = mean[1]
-    theta1 = mean[2]
+    x1 = xi[:, 0]
+    y1 = xi[:, 1]
+    theta1 = xi[:, 2]
 
-    x2 = x1 + dd * cos(theta1)
-    y2 = y1 + dd * sin(theta1)
-    theta2 = theta1 + dt
+    x2 = (x1 + dd * cos(theta1)).reshape((-1, 1))
+    y2 = (y1 + dd * sin(theta1)).reshape((-1, 1))
+    theta2 = (theta1 + dt).reshape((-1, 1))
 
-    cov[0,0] += 1
-    cov[1,1] += 1
-    cov[2,2] += 0.1
-    mean2 = array([x2, y2, theta2]).reshape((3, 1))
-    print(mean)
-    print(mean2)
+    xi_new = concatenate([x2, y2, theta2], axis=1)
 
-    return {'mu': mean2.tolist(), 'sigma': cov.tolist()}
+    mu_new = wm.dot(xi_new)
+    sigma_new = (xi_new - mu_new).T.dot(wc_diag).dot(xi_new - mu_new) + R
+
+    return {'mu': mu_new.reshape((-1, 1)).tolist(),
+            'sigma': sigma_new.tolist(),
+            'type': 'ukf'}
+
 
 # TODO split the robots out into different modules
 # staying here for now
@@ -188,15 +199,24 @@ def movement_update_request(filtertype='ukf'):
     data = request.get_json()
 
     if not data or 'leftwheel' not in data or \
-            'rightwheel' not in data or 'robots' not in data:
+            'rightwheel' not in data or 'robots' not in data or \
+            'R' not in data:
         abort(404)
 
     leftwheel = float(data['leftwheel'])
     rightwheel = float(data['rightwheel'])
+    R = array(data['R'], dtype=float64)
+
     new_positions = []
     for robot in data['robots']:
-        new_positions.append(ukf_movement(
-            leftwheel, rightwheel, robot))
+        if robot['type'] is not None:
+            if robot['type'] == 'ukf':
+                new_positions.append(ukf_movement(
+                    leftwheel, rightwheel, R, robot))
+            elif robot['type'] == 'histogram':
+                new_positions.append(histogram_movement(
+                    leftwheel, rightwheel, R, robot))
+
 
     return jsonify({"robots": new_positions})
 
